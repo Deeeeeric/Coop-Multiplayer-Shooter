d96806f828983593731576bbe5320721d250a533
dericwu <deric.wv18@outlook.com>
2020-06-17 19:14:27 +0100

    Line Tracing pt.2
    Line trace from camera view instead of Pawn Base eye height

--- CoopShooter/Source/CoopShooter/SCharacter.cpp
+++ CoopShooter/Source/CoopShooter/SCharacter.cpp
@@ -83,3 +83,13 @@ void ASCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponen
 	PlayerInputComponent->BindAction("Jump", IE_Pressed, this, &ACharacter::Jump);
 }
 
+FVector ASCharacter::GetPawnViewLocation() const
+{
+	if (CameraComponent)
+	{
+		return CameraComponent->GetComponentLocation();
+	}
+
+	return Super::GetPawnViewLocation();
+}
+
